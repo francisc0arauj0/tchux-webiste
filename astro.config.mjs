e96b22c5fe72dae4f7c73f64fd035876150547fe
francisc0arauj0 <franciscocoelhoaraujo492000@gmail.com>
2025-11-22 19:13:39 +0000

    add tailwind, navbar and new layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {plugins: [tailwindcss()]},
+    integrations: [
+        starlight({
+            title: "Tchux Docs",
+            customCss: ["./src/styles/global.css"],
+            social: [
+				{
+					icon: "github",
+					label: "Github",
+					href: "https://github.com/francisc0arauj0/tchux",
+				},
+			],
+        })
+    ]
+});
